702b77cd3d1c47dd614fcc67a19fa8934a839f74
KarenBaliero <karenbaliero.lima@gmail.com>
2025-07-16 13:50:53 -0300

    Add cmd to init each agent

--- src/main/java/Environment.java
+++ src/main/java/Environment.java
@@ -31,43 +31,30 @@ import ws3dproxy.util.Logger;
  */
 public final class Environment {
     
-    public String host="localhost";
-    public int port = 4011;
     public String robotID="r0";
-    public Creature cDefault = null;
-    public Creature cDefault2 = null;
-    public Creature cJSoar1 = null;
-    public Creature cJSoar2 = null;
+    public Creature c = null;
+    private String growOption;
     
-    public Environment() {
-          WS3DProxy proxy = new WS3DProxy();
-          try {   
-             World w = World.getInstance();
-             w.reset();
-             World.createFood(0, 350, 75);
-             World.createFood(0, 100, 220);
-             World.createFood(0, 250, 210);
-             cDefault = proxy.createCreature(100,450,0,0);
-             cDefault.start();
-             
-             cDefault2 = proxy.createCreature(450,100,0,0);
-             cDefault2.start();
-             
-             cJSoar1 = proxy.createCreature(50,50,0,0);
-             cJSoar1.start();
-             
-             cJSoar2 = proxy.createCreature(550,550,0,0);
-             cJSoar2.start();
-             
-             grow(w,7);
-          } catch (CommandExecException e) {
-              
-          }
-          System.out.println("Robot "+cDefault.getName()+" is ready to go.");
-          System.out.println("Robot "+cDefault2.getName()+" is ready to go.");
-          System.out.println("Robot "+cJSoar1.getName()+" is ready to go.");
-          System.out.println("Robot "+cJSoar2.getName()+" is ready to go.");
-	}
+    public Environment(String growOption) {
+        this.growOption = growOption;
+        WS3DProxy proxy = new WS3DProxy();
+        try {   
+           World w = World.getInstance();
+           w.reset();
+           World.createFood(0, 350, 75);
+           World.createFood(0, 100, 220);
+           World.createFood(0, 250, 210);
+           c = proxy.createCreature(100,450,0,0);
+           c.start();
+           if(!growOption.equals("")) {
+               grow(w,7);
+           }
+           
+        } catch (CommandExecException e) {
+
+        }
+        System.out.println("Robot "+c.getName()+" is ready to go.");
+    }
     
     public synchronized void grow(World w, int time) {
         try {
@@ -75,7 +62,7 @@ public final class Environment {
                 time = Constants.TIMEFRAME;
             }
             w.getDimensionAndDeliverySpot();
-            ResourcesGenerator rg = new ResourcesGenerator(time, w.getEnvironmentWidth(), w.getEnvironmentHeight(), w.getDeliverySpot().getX(), w.getDeliverySpot().getY());
+            ResourcesGenerator rg = new ResourcesGenerator(time, w.getEnvironmentWidth(), w.getEnvironmentHeight(), w.getDeliverySpot().getX(), w.getDeliverySpot().getY(), growOption);
             rg.start();
         } catch (CommandExecException ex) {
             Logger.logException(World.class.getName(), ex);
--- src/main/java/ExperimentMain.java
+++ src/main/java/ExperimentMain.java
@@ -20,47 +20,85 @@
 import br.unicamp.cst.util.viewer.MindViewer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.Random;
 
 /**
  *
  * @author rgudwin
  */
 public class ExperimentMain {
-	
-	
-        
-        public Logger logger = Logger.getLogger(ExperimentMain.class.getName());
-        
-        
-        public ExperimentMain() {
-            Logger.getLogger("codelets").setLevel(Level.SEVERE);
-            // Create Environment
-            Environment env=new Environment(); //Creates only a creature and some apples
-            AgentMindDefault a1 = new AgentMindDefault(env.cDefault, "AgentDefault1");  // Creates the Agent Mind and start it   
-            // The following lines create the MindViewer and configure it 
-            MindViewer mv = new MindViewer(a1,"MindViewer",a1.behavioralCodelets);
-            mv.setVisible(true);
-            
-            AgentMindDefault a2 = new AgentMindDefault(env.cDefault2, "AgentDefault2"); 
-            MindViewer mv2 = new MindViewer(a2,"MindViewer",a2.behavioralCodelets);
-            mv2.setVisible(true);
-            
-            AgentMindJSoar aJsoar1 = new AgentMindJSoar(env.cJSoar1, env.cJSoar1.getName()); 
-            MindViewer mvAJsoar1 = new MindViewer(aJsoar1,"MindViewer  Agent Soar 1", null);
-            mvAJsoar1.setVisible(true);
-            
-            AgentMindJSoar aJsoar2 = new AgentMindJSoar(env.cJSoar2, env.cJSoar2.getName()); 
-            MindViewer mvAJsoar2 = new MindViewer(aJsoar2,"MindViewer Agent Soar 2", null);
-            mvAJsoar2.setVisible(true);
-            
+    public Logger logger = Logger.getLogger(ExperimentMain.class.getName());
+
+    public ExperimentMain(String agentType, String growOption) {
+        Logger.getLogger("codelets").setLevel(Level.SEVERE);
+        // Create Environment
+        Environment env=new Environment(growOption); //Creates only a creature and some apples
+        String creatureName = env.c.getName();
+        switch (agentType) {
+            case "soar":
+                AgentMindJSoar aJsoar1 = new AgentMindJSoar(env.c, env.c.getName()); 
+                MindViewer mvAJsoar1 = new MindViewer(aJsoar1,"MindViewer - Agent Soar " + creatureName, null);
+                mvAJsoar1.setVisible(true);
+                break;
+            case "default":
+                AgentMindDefault a1 = new AgentMindDefault(env.c, "AgentDefault " +  creatureName);
+                MindViewer mv = new MindViewer(a1,"MindViewer - AgentDefault " + creatureName, a1.behavioralCodelets);
+                mv.setVisible(true);
+                break;
         }
+    }
 
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		ExperimentMain em = new ExperimentMain();
-	}
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        /** 
+         * aqui vai pegar o args e de acordo com cada tipo criar um agente diferente
+         * soar, bn, default, vehicle, habits
+         * define se vai usar o grow também com a flag -g food jewel
+         * se for só '-g' ou '-g food jewel', cresce food e jewel - envia a string "grow"
+         * se for só '-g food' cresce food- envia a string "grow-fodd"
+         * se for só '-g jewel' cresce só jewel - envia a string "grow-jewel"
+         * mandar como argumento do ExperimentMain essas informações, um com a string do tipo e um com a string de grow
+        **/
+        String agentType = "default"; 
+        String growOption = "";
+
+        for (int i = 0; i < args.length; i++) {
+            switch (args[i]) {
+                case "soar":
+                case "bn":
+                case "default":
+                case "vehicle":
+                case "habits":
+                    agentType = args[i];
+                    System.out.println("AgentType: " + agentType);
+                    break;
+                case "-g":
+                    if (i + 1 < args.length) {
+                        if ("food".equals(args[i + 1]) && i + 2 < args.length && "jewel".equals(args[i + 2])) {
+                            growOption = "grow";
+                            i += 2;
+                        } else if ("food".equals(args[i + 1])) {
+                            growOption = "grow-food";
+                            i++;
+                        } else if ("jewel".equals(args[i + 1])) {
+                            growOption = "grow-jewel";
+                            i++;
+                        } else {
+                            growOption = "grow"; // default: ativa ambos se nenhum especificado
+                        }
+                    } else {
+                        growOption = "grow";
+                    }
+                    break;
+                default:
+                    System.out.println("Unkown arg: " + args[i]);
+                    break;
+            }
+        }
+            ExperimentMain em = new ExperimentMain(agentType, growOption);
+    }
 
 }
